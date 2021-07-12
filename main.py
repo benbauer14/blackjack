ff1ac3dd6b0e11f490d6dcc9c963a8c8a4c05255
Ben Bauer <ben.bauer1@gmail.com>
2021-07-12 13:25:35 -0500

    dealer logic and hit functions working

--- main.py
+++ main.py
@@ -6,12 +6,23 @@ myCards = []
 dealerCards = []
 total = 0
 
-def yourCards():
-    myCards.append(cards[randomint(0,len(cards) - 1)])
-    myCards.append(cards[randomint(0,len(cards) - 1)])
+def deal():
+    myCards.append(cards[random.randint(0,len(cards) - 1)])
+    myCards.append(cards[random.randint(0,len(cards) - 1)])
+    dealerCards.append(cards[random.randint(0,len(cards) - 1)])
+    dealerCards.append(cards[random.randint(0,len(cards) - 1)])
+    print("Your cards: ")
+    print(myCards)
+    print("Dealer cards: ")
+    firstDealerCard = str(dealerCards[0])
+    print("[" + firstDealerCard + ", _ ]")
 
 def hitme():
-    myCards.append(cards[randomint(0,len(cards) - 1)])
+    myCards.append(cards[random.randint(0,len(cards) - 1)])
+    if(sumCards(myCards) > 21):
+        print("Game Over! You Lose!")
+        print(myCards)
+        print(dealerCards)
 
 def sumCards(cards):
     total = 0
@@ -30,3 +41,18 @@ def sumCards(cards):
 
 
 def dealer():
+    dealerTotal = sumCards(dealerCards)
+    while(dealerTotal < 16):
+        dealerCards.append(cards[random.randint(0,len(cards) - 1)])
+        dealerTotal = sumCards(dealerCards)
+    return dealerTotal
+
+
+deal()
+print(myCards)
+print(dealerCards)
+hitme()
+print(myCards)
+print(sumCards(myCards))
+dealer()
+print(sumCards(dealerCards))
